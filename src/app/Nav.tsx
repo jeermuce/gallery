55a97094f23e2809a7fc6a3378dd3e9f9bc39515
jeermuce <ernesto_mc.dev@proton.me>
2024-06-28 16:34:10 -0600

    ffix script

--- src/app/Nav.tsx
+++ src/app/Nav.tsx
@@ -3,15 +3,15 @@ import Link from "next/link";
 
 function Nav() {
   return (
-    <nav className=" navbar bbg-transparent sticky left-0 top-0 flex h-fit w-full flex-row justify-center bg-opacity-60 text-text-main backdrop-blur-md">
+    <nav className="flex sticky top-0 left-0 flex-row justify-center w-full bg-opacity-60 navbar bbg-transparent h-fit text-text-main backdrop-blur-md">
       <div className="  flex w-[80%] max-w-[80rem] flex-row flex-nowrap items-center justify-between gap-8 overflow-clip py-2 font-semibold">
         <Link href="/">Home</Link>
-        <div className="flex flex-row flex-nowrap justify-center gap-8">
+        <div className="flex flex-row flex-nowrap gap-8 justify-center">
           <Link href="/gallery">Gallery</Link>
           <Link href="/">Home</Link>
           <Link href="/gallery">Gallery</Link>
         </div>
-        <div className="flex flex-row flex-nowrap items-center justify-center gap-8">
+        <div className="flex flex-row flex-nowrap gap-8 justify-center items-center">
           <Link href="/login">Sign In</Link>
           <ThemeSwitcher />
         </div>
