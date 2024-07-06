269445ec04f14fceed5c9f5f22878d65f467948e
jeermuce <ernesto_mc.dev@proton.me>
2024-07-05 21:39:46 -0600

    Add logo

--- src/app/_components/Nav.tsx
+++ src/app/_components/Nav.tsx
@@ -30,8 +30,15 @@ function Nav() {
   }, []);
   return (
     <nav className="flex sticky top-0 left-0 flex-row justify-center w-full bg-transparent navbar z-[9999] h-fit text-text drop-shadow-Srem backdrop-blur-[20px]">
-      <div className="  flex w-[80%] max-w-[80rem] flex-row flex-nowrap items-center justify-between gap-8  py-2 font-semibold">
-        <Link href="/">Gallery</Link>
+      <div className="  flex w-[80%] gap-2 max-w-[80rem] flex-row flex-nowrap items-center justify-between  py-2 font-semibold">
+        <Link href="/" className="flex flex-row flex-nowrap gap-2 items-center">
+          <img
+            src="https://i.imgur.com/FTAK7os.png"
+            className="h-8 aspect-square"
+            alt="Gallery"
+          />
+          <h1>Gallery</h1>
+        </Link>
         {viewportWidth > 542 ? (
           <div className="flex flex-row flex-nowrap gap-8 justify-center">
             <Link href="/dashboard">Dashboard</Link>
