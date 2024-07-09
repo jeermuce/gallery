59fcd147388c830a96e0ff9ad4d07ee21d897a79
jeermuce <ernesto_mc.dev@proton.me>
2024-07-08 22:39:25 -0600

    formatting

--- src/components/FullPageImage.tsx
+++ src/components/FullPageImage.tsx
@@ -23,7 +23,7 @@ export default async function PhotoModal(props: { id: number }) {
       </a>
 
       <div className="min-h-full border-l min-w-[12rem]">
-        <div className="justify-start items-start h-full  flexflex-col">
+        <div className="justify-start items-start h-full flexflex-col">
           <div className="p-2 w-full text-lg font-bold text-center border-b">
             {image?.name ?? "ERROR"}
           </div>
