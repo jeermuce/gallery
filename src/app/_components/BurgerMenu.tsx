d781ce51125ecefd823c62affb1b04be9a79314a
jeermuce <ernesto_mc.dev@proton.me>
2024-07-14 04:04:12 -0600

    Feat: Clicks outside the menu close it

--- src/app/_components/BurgerMenu.tsx
+++ src/app/_components/BurgerMenu.tsx
@@ -5,14 +5,24 @@ import CustomUploadButton from "./CustomUploadButton";
 import ThemeSwitcher from "~/app/_components/ThemeSwitcher";
 const BurgerMenu = () => {
   return (
-    <div className="mustbevisibleonlywhenthescreenisthinerthan542 aspect-square w-[24px] h-[24px] descendants:cursor-pointer">
+    <div
+      className="
+    descendants:transition-all 
+    escendants:ease-out 
+    mustbevisibleonlywhenthescreenisthinerthan542
+    aspect-square 
+    w-[24px] h-[24px] 
+    
+    descendants:duration-300
+    "
+    >
       <input
         type="checkbox"
         defaultChecked={true}
         id="sidebar-toggle"
-        className="hidden fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 peer/invisible"
+        className=" fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 peer/invisible"
       />
-      <label htmlFor="sidebar-toggle" className="descendants:size-[24px]">
+      <label htmlFor="sidebar-toggle" className="cursor-pointer">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           role="menu"
@@ -31,11 +41,18 @@ const BurgerMenu = () => {
           <line x1="3" y1="18" x2="21" y2="18" />
         </svg>
       </label>
-      <div className="flex fixed top-0 right-0 flex-col gap-2 justify-start items-end h-screen rounded-l-lg transition-all ease-in-out sidebar-container text-wrap bg-black/60 backdrop-blur-lg peer-checked/invisible:-right-full w-fit duration-400">
+      <label
+        htmlFor="sidebar-toggle"
+        className="w-screen h-screen peer-checked/invisible:hidden  fixed top-0 left-0 z-9 "
+      />
+      <div className="z-index-10 flex fixed top-0 right-0 flex-col gap-2 justify-start items-end h-screen rounded-l-lg transition-all ease-in-out sidebar-container text-wrap bg-black/60 backdrop-blur-lg peer-checked/invisible:-right-full w-fit duration-400">
         <div className="flex flex-col gap-2 justify-start items-center p-12 text-white bg-transparent">
           <div className="flex fixed top-0 left-0 flex-row justify-between w-full p-[11px] descendants:size-[24px]">
             <ThemeSwitcher />
-            <label htmlFor="sidebar-toggle">
+            <label
+              htmlFor="sidebar-toggle"
+              className="hover:text-secondary cursor-pointer"
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 role="menu"
