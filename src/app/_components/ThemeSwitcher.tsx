8416ccc1cbb7fa5f42165876452882385fed36d6
jeermuce <ernesto_mc.dev@proton.me>
2024-07-14 00:57:27 -0600

    Remove bad placeholder

--- src/app/_components/ThemeSwitcher.tsx
+++ src/app/_components/ThemeSwitcher.tsx
@@ -15,22 +15,20 @@ export default function ThemeSwitch(
 
   if (!mounted)
     return (
-      <Image
-        src="data:image/svg+xml;base64,PHN2ZyBzdHJva2U9IiNGRkZGRkYiIGZpbGw9IiNGRkZGRkYiIHN0cm9rZS13aWR0aD0iMCIgdmlld0JveD0iMCAwIDI0IDI0IiBoZWlnaHQ9IjIwMHB4IiB3aWR0aD0iMjAwcHgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjIwIiBoZWlnaHQ9IjIwIiB4PSIyIiB5PSIyIiBmaWxsPSJub25lIiBzdHJva2Utd2lkdGg9IjIiIHJ4PSIyIj48L3JlY3Q+PC9zdmc+Cg=="
-        width={36}
-        height={36}
-        sizes="36x36"
-        alt="Loading Light/Dark Toggle"
-        priority={false}
+      <FiSun
+        width={24}
+        height={24}
         title="Loading Light/Dark Toggle"
+        className="cursor-not-allowed"
       />
     );
 
   return (
     <button
       type="button"
+      title="Toggle Light/Dark Mode"
       {...props}
-      className={`${props.className} flex justify-center items-center `}
+      className={`${props.className} descendants:cursor-pointer flex justify-center items-center descendants:size-[20px] `}
       onMouseDown={() =>
         setTheme(() => (resolvedTheme === "dark" ? "light" : "dark"))
       }
