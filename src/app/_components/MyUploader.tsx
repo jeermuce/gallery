ca691d7a9160a963414e78593fb3255fa04a1d9b
jeermuce <ernesto_mc.dev@proton.me>
2024-07-16 04:08:22 -0600

    Fix flex-1 issue

--- src/app/_components/MyUploader.tsx
+++ src/app/_components/MyUploader.tsx
@@ -7,7 +7,7 @@ function MyUploader() {
   const router = useRouter();
 
   return (
-    <div className="flex flex-col justify-center items-center pt-20 w-4/5 h-full rounded-2xl">
+    <div className="flex flex-col flex-1 justify-center items-center py-20 w-4/5 h-full rounded-2xl">
       <Uploader
         onClientUploadComplete={() => router.refresh()}
         endpoint="imageUploader"
